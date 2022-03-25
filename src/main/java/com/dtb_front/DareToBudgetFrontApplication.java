fb60c4664e31a695390dfe49e4d1689080a86240
donzdanowicz <madpz89@gmail.com>
2022-03-25 22:14:30 +0100

    First commit

---
+++ src/main/java/com/dtb_front/DareToBudgetFrontApplication.java
@@ -0,0 +1,13 @@
+package com.dtb_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DareToBudgetFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DareToBudgetFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dtb_front/DareToBudgetFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtb_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DareToBudgetFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
